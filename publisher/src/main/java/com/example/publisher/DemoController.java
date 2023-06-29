39157902f1af7d9047a97e327ae567a5ec85bb31
ikeda-mk <2306915+ikeda-mk@users.noreply.github.com>
2023-06-29 20:58:46 +0900

    spring boot 3.1 に移行

--- publisher/src/main/java/com/example/publisher/DemoController.java
+++ publisher/src/main/java/com/example/publisher/DemoController.java
@@ -2,30 +2,29 @@ package com.example.publisher;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@EnableBinding(Source.class)
+
 public class DemoController {
-    private final Source source;
+    private final StreamBridge streamBridge;
 
     private final Logger log = LoggerFactory.getLogger(DemoController.class);
 
-    public DemoController(Source source) {
-        this.source = source;
+    public DemoController(StreamBridge streamBridge) {
+        this.streamBridge = streamBridge;
     }
 
     // curl -v localhost:8080 -d '{"text":"Hello"}' -H 'Content-Type: application/json'
     @PostMapping
     public void tweet(@RequestBody Tweet tweet) {
         log.info("publish {}", tweet.text);
-        source.output().send(MessageBuilder.withPayload(tweet).build());
+        streamBridge.send("output", tweet);
+
     }
 
     public static class Tweet {
--- subscriber/src/main/java/com/example/subscriber/MessageListener.java
+++ subscriber/src/main/java/com/example/subscriber/MessageListener.java
@@ -1,22 +1,30 @@
 package com.example.subscriber;
 
+import java.util.function.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
 
-@EnableBinding(Sink.class)
-public class MessageListener {
+@Component("sandboxConsumer")
+public class MessageListener implements Consumer<Message<MessageListener.Tweet>>{
 
     private final Logger log = LoggerFactory.getLogger(MessageListener.class);
 
-    @StreamListener(Sink.INPUT)
-    public void print(Tweet tweet) {
-        log.info("Received {}", tweet.text);
+    @Override
+    public void accept(Message<Tweet> tweet) {
+        log.info("Received {}", tweet.getPayload().getText());
     }
 
     public static class Tweet {
-        public String text;
+        private String text;
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
     }
 }
